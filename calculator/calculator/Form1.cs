a182f08a4834e5ad83b5d5e14493b363ba0676d7
707BIGBOSS\user <2001mist@naver.com>
2022-09-20 20:50:35 +0900

    C# 계산기 03 사칙연산, C, CE, dot, ...

--- calculator/calculator/Form1.cs
+++ calculator/calculator/Form1.cs
@@ -13,8 +13,7 @@ namespace calculator
         public enum Operators { sum, sub, multi, div, equal }
     public partial class Form1 : Form
     {
-        public double num1 = 0;
-        public double num2;
+        
         public double Result = 0;
         public bool newNum = true;
         public Operators op = Operators.sum;
@@ -49,32 +48,102 @@ namespace calculator
 
         private void Add_click(object sender, EventArgs e)
         {
+            // 사칙연산자 저장
+            Button opBtn = (Button)sender;
+            if (opBtn.Text == "+")
+            {
+                op = Operators.sum;
+                newNum = false;
+            }
+            else if (opBtn.Text == "-")
+            {
+                op = Operators.sub;
+                newNum = false;
+            }
+            else if (opBtn.Text == "x")
+            {
+                op = Operators.multi;
+                newNum = false;
+            }
+            else if (opBtn.Text == "÷")
+            {
+                op = Operators.div;
+                newNum = false;
+            }
+            else if (opBtn.Text == "=")
+            {
+                op = Operators.equal;
+            }
 
+            // ***사칙연산: A (연산자) B = 결과 ***
+            // 계산 + - * / = 
+            // ResultExist = true;
+            if (newNum == false)
+            {
+                double num = double.Parse(screen1.Text);
+                if (op == Operators.sum)
+                {
+                    Result = Result + num;
+                }
+                else if (op == Operators.sub)
+                {
+                    Result = Result - num;
+                }
+                else if (op == Operators.multi)
+                {
+                    Result = Result * num;
+                }
+                else if (op == Operators.div)
+                {
+                    Result = Result / num;
+                }
+                screen1.Text = Result.ToString();
+                newNum = true;
+            }
+
+            screen2.Text = opBtn.Text;
         }
 
         private void back_Click(object sender, EventArgs e)
         {
-
+            screen1.Text = screen1.Text.Substring(0, screen1.Text.Length - 1);
         }
 
         private void clean_Click(object sender, EventArgs e)
         {
+            Result = 0;
+            newNum = true;
+            op = Operators.sum;
 
+            screen1.Text = "0";
+            screen2.Text = "";
         }
 
         private void CE_Click(object sender, EventArgs e)
         {
-
+            screen1.Text = "0";
         }
 
         private void dot_Click(object sender, EventArgs e)
         {
-
+            if (screen1.Text.Contains("."))
+            {
+                return;
+            }
+            else
+            {
+                screen1.Text = screen1.Text + ".";
+            }
         }
 
         private void plusMinus_Click(object sender, EventArgs e)
         {
+            double pmBtn = double.Parse(screen1.Text);
 
+            pmBtn = -pmBtn;
+            Result = -Result;
+
+            screen1.Text = pmBtn.ToString();
         }
     }
 }
